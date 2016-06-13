450f271e1c4ff8c345258c393df3c47414cd6e7a
ryanYang17 <keepryan0610@163.com>
2016-06-13 23:37:37 +0800

    提交向指定邮箱发送邮件代码

--- Manage/ValetSafeManagement/ValetSafeManagement/Program.cs
+++ Manage/ValetSafeManagement/ValetSafeManagement/Program.cs
@@ -10,6 +10,25 @@ namespace ValetSafeManagement
     {
         static void Main(string[] args)
         {
+            try
+            {
+                //smtp.163.com
+                string senderServerIp = "123.125.50.133";
+                string toMailAddress = "keepryan0610@163.com";
+                string fromMailAddress = "lihaifighting@163.com";
+                string subjectInfo = "Test sending e_mail";
+                string bodyInfo = "Hello Eric, This is my first testing e_mail";
+                string mailUsername = "lihaifighting";
+                string mailPassword = "SANmeimuZI17*"; //发送邮箱的密码（）
+                string mailPort = "25";
+
+                MyEmail email = new MyEmail(senderServerIp, toMailAddress, fromMailAddress, subjectInfo, bodyInfo, mailUsername, mailPassword, mailPort, false, false);
+                email.Send();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
--- Manage/ValetSafeManagement/ValetSafeManagement/PublicFunction.cs
+++ Manage/ValetSafeManagement/ValetSafeManagement/PublicFunction.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.IO;
+using System.Timers;
+using System.Xml;
 
 namespace ValetSafeManagement
 {
@@ -59,6 +65,80 @@ namespace ValetSafeManagement
                     ResultStr.Add(fileArray[i]);
                 }
                 return ResultStr;
+            }
+        }
+        
+    }
+
+    public class MyEmail
+    {
+        private MailMessage mMailMessage;   //主要处理发送邮件的内容（如：收发人地址、标题、主体、图片等等）
+        private SmtpClient mSmtpClient; //主要处理用smtp方式发送此邮件的配置信息（如：邮件服务器、发送端口号、验证方式等等）
+        private int mSenderPort;   //发送邮件所用的端口号（htmp协议默认为25）
+        private string mSenderServerHost;    //发件箱的邮件服务器地址（IP形式或字符串形式均可）
+        private string mSenderPassword;    //发件箱的密码
+        private string mSenderUsername;   //发件箱的用户名（即@符号前面的字符串，例如：hello@163.com，用户名为：hello）
+        private bool mEnableSsl;    //是否对邮件内容进行socket层加密传输
+        private bool mEnablePwdAuthentication;  //是否对发件人邮箱进行密码验证
+
+
+        public MyEmail(string server, string toMail, string fromMail, string subject, string emailBody, string username, string password, string port, bool sslEnable, bool pwdCheckEnable)
+        {
+            try
+            {
+                mMailMessage = new MailMessage();
+                mMailMessage.To.Add(toMail);
+                mMailMessage.From = new MailAddress(fromMail);
+                mMailMessage.Subject = subject;
+                mMailMessage.Body = emailBody;
+                mMailMessage.IsBodyHtml = true;
+                mMailMessage.BodyEncoding = System.Text.Encoding.UTF8;
+                mMailMessage.Priority = MailPriority.Normal;
+                this.mSenderServerHost = server;
+                this.mSenderUsername = username;
+                this.mSenderPassword = password;
+                this.mSenderPort = Convert.ToInt32(port);
+                this.mEnableSsl = sslEnable;
+                this.mEnablePwdAuthentication = pwdCheckEnable;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        ///<summary>
+        /// 邮件的发送
+        ///</summary>
+        public void Send()
+        {
+            try
+            {
+                if (mMailMessage != null)
+                {
+                    mSmtpClient = new SmtpClient();
+                    //mSmtpClient.Host = "smtp." + mMailMessage.From.Host;
+                    mSmtpClient.Host = this.mSenderServerHost;
+                    mSmtpClient.Port = this.mSenderPort;
+                    mSmtpClient.UseDefaultCredentials = false;
+                    mSmtpClient.EnableSsl = this.mEnableSsl;
+                    if (this.mEnablePwdAuthentication)
+                    {
+                        System.Net.NetworkCredential nc = new System.Net.NetworkCredential(this.mSenderUsername, this.mSenderPassword);
+                        //mSmtpClient.Credentials = new System.Net.NetworkCredential(this.mSenderUsername, this.mSenderPassword);
+                        //NTLM: Secure Password Authentication in Microsoft Outlook Express
+                        mSmtpClient.Credentials = nc.GetCredential(mSmtpClient.Host, mSmtpClient.Port, "NTLM");
+                    }
+                    else
+                    {
+                        mSmtpClient.Credentials = new System.Net.NetworkCredential(this.mSenderUsername, this.mSenderPassword);
+                    }
+                    mSmtpClient.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                    mSmtpClient.Send(mMailMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
             }
         }
     }
