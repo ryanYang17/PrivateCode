1cebeeab68ead0ad408435b5fcea35381dfec9d0
zhenya huang <huang3644129@163.com>
2016-06-28 14:15:17 +0800

    更新NetworkService
    对应服务器提交addRegisterUser方法

--- ValetSafeDroid/ValetSafeDroid/app/src/main/java/service/NetworkService.java
+++ ValetSafeDroid/ValetSafeDroid/app/src/main/java/service/NetworkService.java
@@ -35,7 +35,7 @@ public class NetworkService {
         Map<String, String> return_data = new HashMap<>();
         try {
             CBConnection connection = CBHttp.getInstance();
-            String baseURL = "http://47.88.192.36:8080/testhy/addSignUpUser";
+            String baseURL = "http://47.88.192.36:8080/valetsafe/addRegisterUser";
             CBPrint.println(baseURL);
             result = connection.connect(baseURL).method(CBMethod.POST).timeout(5000).data(data).execute();
             return_data.put("result", result);
