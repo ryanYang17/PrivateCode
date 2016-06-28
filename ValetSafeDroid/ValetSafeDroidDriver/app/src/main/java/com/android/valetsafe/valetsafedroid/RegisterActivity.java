9ee99e40aff6e517c6e2d60269b9220367a3db25
ryanYang17 <keepryan0610@163.com>
2016-06-28 10:23:19 +0800

    1

--- ValetSafeDroid/ValetSafeDroidDriver/app/src/main/java/com/android/valetsafe/valetsafedroid/RegisterActivity.java
+++ ValetSafeDroid/ValetSafeDroidDriver/app/src/main/java/com/android/valetsafe/valetsafedroid/RegisterActivity.java
@@ -2,12 +2,23 @@ package com.android.valetsafe.valetsafedroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText phone;
+    private EditText mail;
+    private EditText password;
+    private EditText repassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
+        name = (EditText)findViewById(R.id.register_edit_name);
+        phone = (EditText)findViewById(R.id.register_edit_phone);
+        mail = (EditText)findViewById(R.id.register_edit_email);
+        password = (EditText)findViewById(R.id.register_edit_password);
+        repassword = (EditText)findViewById(R.id.register_edit_repassword);
     }
 }
