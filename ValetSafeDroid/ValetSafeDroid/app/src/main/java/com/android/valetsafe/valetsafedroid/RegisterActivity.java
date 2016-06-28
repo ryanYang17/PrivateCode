e45fec58a2e7f9819c7a04a320cf28fe26d73d65
ryanYang17 <keepryan0610@163.com>
2016-06-28 11:16:35 +0800

    change

--- ValetSafeDroid/ValetSafeDroid/app/src/main/java/com/android/valetsafe/valetsafedroid/RegisterActivity.java
+++ ValetSafeDroid/ValetSafeDroid/app/src/main/java/com/android/valetsafe/valetsafedroid/RegisterActivity.java
@@ -2,12 +2,34 @@ package com.android.valetsafe.valetsafedroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText phone;
+    private EditText mail;
+    private EditText password;
+    private EditText repassword;
+//    private ImageView duihaoFrame;
+//    private ImageView duihao;
+    private int duihaoCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
+        name = (EditText)findViewById(R.id.register_edit_name);
+        phone = (EditText)findViewById(R.id.register_edit_phone);
+        mail = (EditText)findViewById(R.id.register_edit_email);
+        password = (EditText)findViewById(R.id.register_edit_password);
+        repassword = (EditText)findViewById(R.id.register_edit_repassword);
+        //duihaoFrame = (ImageView)findViewById(R.id.register_view_set);
+       // duihao = (ImageView)findViewById(R.id.register_view_noset);
+
+
+
+
     }
 }
--- ValetSafeDroid/ValetSafeDroidDriver/app/src/main/java/com/android/valetsafe/valetsafedroid/RegisterActivity.java
+++ ValetSafeDroid/ValetSafeDroidDriver/app/src/main/java/com/android/valetsafe/valetsafedroid/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.android.valetsafe.valetsafedroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText name;
@@ -10,6 +11,11 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mail;
     private EditText password;
     private EditText repassword;
+    private ImageView duihaoFrame;
+    private ImageView duihao;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,8 @@ public class RegisterActivity extends AppCompatActivity {
         mail = (EditText)findViewById(R.id.register_edit_email);
         password = (EditText)findViewById(R.id.register_edit_password);
         repassword = (EditText)findViewById(R.id.register_edit_repassword);
+        duihaoFrame = (ImageView)findViewById(R.id.register_view_set);
+        duihao = (ImageView)findViewById(R.id.register_view_noset);
+
     }
 }
