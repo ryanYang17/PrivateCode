31d048549a7d2841b7147ceebea299282061e502
ryanYang17 <keepryan0610@163.com>
2016-07-06 17:10:48 +0800

    Revert "客户端代码"
    This reverts commit 88940d8fc9f81dbbf67c92591b0e8d7b5d113a29.

--- ValetSafeDroid/ValetSafeDroid/app/src/main/java/com/android/valetsafe/valetsafedroid/SettingActivity.java
+++ ValetSafeDroid/ValetSafeDroid/app/src/main/java/com/android/valetsafe/valetsafedroid/SettingActivity.java
@@ -1,44 +1,18 @@
 package com.android.valetsafe.valetsafedroid;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
-import java.util.Map;
-import com.android.valetsafe.valetsafedroid.PublicFunction;
-
-import service.NetworkService;
 
 public class SettingActivity extends AppCompatActivity {
-    public final static int RESULT_CODE=1;
     private ImageView backBtn;
-    private ImageView ModifyName;
-    private ImageView ModifyCellphone;
-    private ImageView ModifyEmail;
-    private EditText NameText;
-    private EditText PhoneText;
-    private EditText EmailText;
-    private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_layout);
         backBtn = (ImageView) findViewById(R.id.setting_back_btn);
-        ModifyName = (ImageView) findViewById(R.id.imageView11);
-        ModifyCellphone = (ImageView) findViewById(R.id.imageView12);
-        ModifyEmail = (ImageView) findViewById(R.id.imageView13);
-        NameText = (EditText) findViewById(R.id.editText4);
-        PhoneText = (EditText) findViewById(R.id.editText5);
-        EmailText = (EditText) findViewById(R.id.editText6);
 
-        ModifyName.setOnClickListener(new ButtonClickListener());
-        ModifyCellphone.setOnClickListener(new ButtonClickListener());
-        ModifyEmail.setOnClickListener(new ButtonClickListener());
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,66 +20,5 @@ public class SettingActivity extends AppCompatActivity {
 
             }
         });
-        handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                Toast.makeText(SettingActivity.this, "abc" + msg.getData().getString("result"), Toast.LENGTH_SHORT).show();
-                super.handleMessage(msg);
-            }
-        };
-    }
-    class ButtonClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.imageView11:
-                case R.id.imageView12:
-                case R.id.imageView13:
-                    new Thread() {
-                        @Override
-                        public void run() {
-                            String name = NameText.getText().toString();
-                            String cell_phone = PhoneText.getText().toString();
-                            String email = EmailText.getText().toString();
-                            PublicFunction valid = new PublicFunction();
-                            if (!valid.ValidateUserName(name))
-                            {
-                                NameText.setError("user name can't be empty!");
-                                NameText.setText("");
-                            }
-                            else if (!valid.ValidateCellphone(cell_phone))
-                            {
-                                PhoneText.setError("Invalid cellphone number!");
-                                PhoneText.setText("");
-                            }
-                            else if (!valid.ValidateEmail(email))
-                            {
-                                EmailText.setError("Invalid Email address!");
-                                EmailText.setText("");
-                            }
-                            else
-                            {
-                                //调用网络服务进行注册用户操作
-                                NetworkService service = new NetworkService();
-                                Map<String, String> data = service.SetPassengerSetting(name,cell_phone,email);
-                                String result = data.get("result");
-                                Message msg = new Message();
-                                msg.arg1 = 0;
-                                msg.getData().putString("result", result);
-                                handler.sendMessage(msg);
-                                onEnd();
-                            }
-                        }
-                    }.start();
-                    break;
-            }
-        }
-    }
-
-    private void onEnd(){
-        Intent intent=new Intent();
-        intent.putExtra("back", "Back Data");
-        setResult(RESULT_CODE, intent);
-        finish();
     }
 }
--- ValetSafeDroid/ValetSafeDroid/app/src/main/java/service/NetworkService.java
+++ ValetSafeDroid/ValetSafeDroid/app/src/main/java/service/NetworkService.java
@@ -46,33 +46,4 @@ public class NetworkService {
         return return_data;
     }
 
-    /**
-     * 个人设置
-     * @param name 用户名
-     * @param cell_phone 用户手机号
-     * @param email 用户email
-     * @return 返回结果 Map结构。
-     */
-    public Map<String, String> SetPassengerSetting(String name, String cell_phone, String email){
-        String result = null;
-
-        // 构造传输给服务器的消息，与数据库结构一致。
-        Map<String,String> data = new HashMap<String, String>();
-        data.put("name", name);
-        data.put("cell_phone", cell_phone);
-        data.put("email", email);
-
-        Map<String, String> return_data = new HashMap<>();
-        try {
-            CBConnection connection = CBHttp.getInstance();
-            String baseURL = "http://47.88.192.36:8080/valetsafe/PassengerSet";
-            CBPrint.println(baseURL);
-            result = connection.connect(baseURL).method(CBMethod.POST).timeout(5000).data(data).execute();
-            return_data.put("result", result);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return return_data;
-    }
 }
