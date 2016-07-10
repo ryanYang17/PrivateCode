6c735f1d9f33cf14622f32ec9e33339dfb100367
ryanYang17 <keepryan0610@163.com>
2016-07-10 23:50:38 +0800

    genxin layout

--- ValetSafeDroid/ValetSafeDroid/app/src/main/java/com/android/valetsafe/valetsafedroid/MainMapFragment.java
+++ ValetSafeDroid/ValetSafeDroid/app/src/main/java/com/android/valetsafe/valetsafedroid/MainMapFragment.java
@@ -141,6 +141,7 @@ public class MainMapFragment extends Fragment implements OnMapReadyCallback {
     public interface OnMainMapFragmentInteractionListener {
         // TODO: Update argument type and name
         void onMainMapFragmentNextBtn();
+
     }
 
     @Override
